c6aaa84b79b8b1f14672e995668d33adab997731
Abhinandan-KP <abhirop538@gmail.com>
2026-03-18 00:26:20 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
+    
     container: {
       center: true,
       padding: "2rem",
